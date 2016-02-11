4fcd86600b3dbb38683fed5db59423570abfaf2e
RR4 <riskreturn5@gmail.com>
2016-02-11 15:02:18 +0900

    map部分のメンテナンスなど

--- PyJSF.py
+++ PyJSF.py
@@ -3,46 +3,35 @@
 # python 3.5
 import urllib.request
 import pandas as pd
-import datetime
 
 
 class PyJSF:
     """
     Pythonで日本証券金融の品貸料、信用残高を取得するクラス
     """
-    def __init__(self, date=datetime.datetime.today()):
+    def __init__(self, date):
+        date = pd.to_datetime(date)
         k = ['y', 'm', 'd']
-        v = [str(date.year), str(date.month).zfill(2), str(date.day).zfill(2)]
+        v = [str(v).zfill(2) for v in [date.year, date.month, date.day]]
         self.args = dict(zip(k, v))
 
-    def shina(self):
-        # URL生成
-        url = 'http://www.jsf.co.jp/de/stock/dlcsv.php?target=pcsl&date={y}-{m}-{d}'.format(**self.args)
+    def __fetch(self, target):
+        self.args['target'] = target
+        url = 'http://www.jsf.co.jp/de/stock/dlcsv.php?target={target}&date={y}-{m}-{d}'.format(**self.args)
         response = urllib.request.urlopen(url)
-
-        # データを取得しDataFrameに追加
         try:
             df = pd.read_csv(response, encoding='Shift_JIS', skiprows=3)
-        except pd.parser.CParserError:
+        except(pd.parser.CParserError):
             df = pd.DataFrame()
-
         return df
+
+    def shina(self):
+        return self.__fetch('pcsl')
 
     def balance(self):
-        # URL生成
-        url = 'http://www.jsf.co.jp/de/stock/dlcsv.php?target=balance&date={y}-{m}-{d}'.format(**self.args)
-        response = urllib.request.urlopen(url)
-
-        # データを取得しDataFrameに追加
-        try:
-            df = pd.read_csv(response, encoding='Shift_JIS', skiprows=3)
-        except pd.parser.CParserError:
-            df = pd.DataFrame()
-
-        return df
+        return self.__fetch('balance')
 
 if __name__ == '__main__':
-    d = datetime.datetime(2016, 1, 4)
-    myJSF = PyJSF(d)
+    myJSF = PyJSF('2016-02-09')
     print(myJSF.shina())    # 品貸料率取得
     print(myJSF.balance())  # 信用残高取得
